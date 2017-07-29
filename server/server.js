6ff7f47ecbdd529d21b019e0435e6465c7a36529
Madhu V Rao <mail2madhva@gmail.com>
2017-07-29 14:50:26 +1000

    seperate test and dev environment

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +9,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 var app = express();
 
