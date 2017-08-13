3a6aa4ed8a92f7d3a528a1c1953c313f26e88a15
Madhu V Rao <mail2madhva@gmail.com>
2017-08-13 16:15:49 +1000

    Authenticate funciton added

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 const port = process.env.PORT;
 
@@ -127,6 +128,10 @@ app.post('/user', (req, res) => {
   }).catch((e) => {
       res.status(400).send(e);
   });
+});
+
+app.get('/user/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 
