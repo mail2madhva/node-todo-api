2d40655c727cdfb319aa6fa91f06a4e1fe513df5
Madhu V Rao <mail2madhva@gmail.com>
2017-10-05 21:40:04 +1100

    hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+      console.log(hash);
+  });
+});
 
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var hashedPassword = '$2a$10$EXunscxCPirpffC08TUOn.Sl7dYD37t31tFiOrW.opBle5qlEt3U.';
 
- var decoded  = jwt.verify(token, '123abc');
- console.log('decoded ', decoded);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
+//
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+//  var decoded  = jwt.verify(token, '123abc');
+//  console.log('decoded ', decoded);
 
 
 // var message = 'I am user number 3';
